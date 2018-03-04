fd26e7c4922d9d7c0f850fc4354ede77e84bb23c
Sergey Rudachenko <m1no.serg@gmail.com>
2018-03-05 00:48:15 +0300

    rollbar notification engine

---
+++ src/Component/Logger/NotificationEngine/Rollbar.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Locaty\Component\Logger\NotificationEngine;
+
+use Rollbar\RollbarLogger;
+use Rollbar\Payload\Level;
+
+class Rollbar extends Basic {
+
+    /**
+     * @var RollbarLogger
+     */
+    private $_rollbarLogger;
+
+    /**
+     * @param string $token
+     * @param string $environment
+     */
+    public function __construct(string $token, string $environment = 'production') {
+        $this->_rollbarLogger = new RollbarLogger([
+            'access_token' => $token,
+            'environment' => $environment,
+        ]);
+    }
+
+    /**
+     * @param \Throwable $e
+     */
+    public function notifyException(\Throwable $e): void {
+        $this->_rollbarLogger->log(Level::ERROR, $e, []);
+    }
+
+    /**
+     * @param string $message
+     * @param array $data
+     */
+    public function notifyMessage(string $message, array $data = []): void {
+        $this->_rollbarLogger->info($message, $data);
+    }
+}
